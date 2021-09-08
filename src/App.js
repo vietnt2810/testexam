74f05e886527252f8a777d490881f4e20f611477
vietnt2810 <viet.nt2810@gmail.com>
2021-09-08 18:55:41 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import customHook from "./hooks/customHook";
 function App() {
+  const [user, index, nextPage, previousPage] = customHook();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>学生一覧：[Huyen, Hoa, Hung, Long]</h4>
+      <h4>位置：{user[index].id}</h4>
+      <h4>名前：{user[index].name}</h4>
+      <button type="" onClick={nextPage}>
+        次に
+      </button>
+      <button type="" style={{ marginLeft: 20 }} onClick={previousPage}>
+        前に
+      </button>
     </div>
   );
 }
